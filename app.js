91a5a99115d3942f370ea97a04a6fd13a1ad7353
Finn <89312393+finnzxje@users.noreply.github.com>
2025-07-14 13:06:06 +0700

    push dist for deployement

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
-import { express } from 'express'
+const express = require('express')
 const app = express()
 
 // get the port from env variable
 const PORT = process.env.PORT || 5000
 
 app.use(express.static('dist'))
+
+app.get('/version', (_req, res) => {
+  res.send('1') // change this string to ensure a new version deployed
+})
 
 app.listen(PORT, () => {
   console.log(`server started on port ${PORT}`)
