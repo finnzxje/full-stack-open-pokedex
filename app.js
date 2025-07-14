d6a94138c1295f67912bd65d9c3122d125b181bf
Finn <89312393+finnzxje@users.noreply.github.com>
2025-07-14 15:25:03 +0700

    stimulate broken deployment

--- app.js
+++ app.js
@@ -7,13 +7,14 @@ const PORT = process.env.PORT || 5000
 app.use(express.static('dist'))
 
 app.get('/version', (_req, res) => {
-  res.send('1') // change this string to ensure a new version deployed
+  res.send('1.1') // change this string to ensure a new version deployed
 })
 
 app.get('/health', (req, res) => {
+  // eslint-disable-next-line no-constant-condition
+  if (true) throw 'error...  '
   res.send('ok')
 })
-
 app.listen(PORT, () => {
   console.log(`server started on port ${PORT}`)
 })
